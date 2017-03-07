c6fa5adfa84be945829b15c39fd7f1f97d5afa18
luyu <luyu@situdata.com>
2017-03-07 15:49:34 +0800

    remove generat mybatis

--- src/test/java/com/situdata/service/impl/TestMyBatis.java
+++ src/test/java/com/situdata/service/impl/TestMyBatis.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.alibaba.fastjson.JSON;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
+@RunWith(SpringJUnit4ClassRunner.class)		//SpringJUnit4ClassRunner
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})
 
 public class TestMyBatis {
